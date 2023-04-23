ac506060e35210ac21bdea647d36d75d9a6fef93
Goen Hee Han <gksrjsgml13@naver.com>
2023-04-23 17:39:44 +0900

    오브젝트 매니저 추가

---
+++ Assets/Scripts/Manager/ObjectManager.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ObjectManager : Singleton<ObjectManager>
+{
+    public GameObject GetObject<T>(Action callback = null)
+    {
+        string path = "Assets/Prefabs/" + typeof(T).Name + ".prefab"; // UI 프리팹 경로
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+        GameObject Obj = Instantiate(prefab);
+
+        callback?.Invoke();
+        return Obj;
+    }
+}
