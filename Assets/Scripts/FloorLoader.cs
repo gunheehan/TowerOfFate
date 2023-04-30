42adaf56cf8231a0b9121a00b0572459caf246bf
Goen Hee Han <gksrjsgml13@naver.com>
2023-04-30 16:05:13 +0900

    floor 오브젝트 parent 추가

--- Assets/Scripts/FloorLoader.cs
+++ Assets/Scripts/FloorLoader.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FloorLoader : MonoBehaviour
 {
+    private GameObject floorContents = null;
     private GameObject floorPrefab = null; // 1x1 크기의 바닥 오브젝트 프리팹
 
     public void CreateFloor(int floorLv)
@@ -17,7 +18,7 @@ public class FloorLoader : MonoBehaviour
             for (int z = 0; z < floorLv; z++)
             {
                 Vector3 position = startPosition + new Vector3(x, 0, z);
-                GameObject floorObject = Instantiate(GetFloorPrefab(), position, Quaternion.identity);
+                GameObject floorObject = Instantiate(GetFloorPrefab(), position, Quaternion.identity, floorContents.transform);
                 
                 if (x >= 1 && x <= floorSize && z >= 1 && z <= floorSize)
                     floorObject.GetComponent<Renderer>().material.color = Color.blue;
@@ -33,6 +34,9 @@ public class FloorLoader : MonoBehaviour
     {
         if (floorPrefab != null)
             return floorPrefab;
+
+        floorContents = new GameObject();
+        floorContents.name = "FloorContents";
         
         floorPrefab = ObjectManager.Instance.GetObject<FloorObject>();
         floorPrefab.SetActive(false);
