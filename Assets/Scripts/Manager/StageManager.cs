497fda2755c268a26739766785b8591624d9c019
Goen Hee Han <gksrjsgml13@naver.com>
2023-04-30 15:51:46 +0900

    스테이지 매니저 추가

---
+++ Assets/Scripts/Manager/StageManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : Singleton<StageManager>
+{
+    private FloorLoader floorLoader = null;
+    
+    public int stageLevel { get; private set; }
+
+    private void Awake()
+    {
+        stageLevel = 0;
+        floorLoader = new FloorLoader();
+    }
+
+    public void OnLoadStage(int stageLv)
+    {
+        SetFloor(stageLv);
+        stageLevel = stageLv;
+    }
+
+    private void SetFloor(int Lv)
+    {
+        int floorSize = 2 * Lv + 1;
+        floorLoader.CreateFloor(floorSize);
+    }
+}
