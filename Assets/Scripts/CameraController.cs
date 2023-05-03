6405369d246981538e93ff2305a5d14d618a5b74
Goen Hee Han <gksrjsgml13@naver.com>
2023-05-03 13:34:59 +0900

    CameraCointroller 스크립트 추가(카메라 및 레이어 관리용)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private RaycastHit rayHit;
+    private Ray ray;
+    
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out rayHit, 50, 1<<LayerMask.NameToLayer("Floor")))
+            {
+                {
+                    CheckPlaceItem(rayHit.transform.gameObject);
+                    Debug.Log(rayHit.transform.name);
+                }
+                Debug.DrawLine(ray.origin,rayHit.point,Color.magenta);
+            }
+            else
+            {
+                Debug.DrawLine(ray.origin,rayHit.point,Color.cyan);
+            }
+        }
+    }
+
+    private void CheckPlaceItem(GameObject floor)
+    {
+        FloorController floorController = floor.GetComponent<FloorController>();
+        Debug.Log(floorController.IsCanPlaced);
+    }
+    
+    public void SetCameraPosition(int level)
+    {
+        // 바닥의 가운데 포지션을 구합니다.
+        Vector3 floorCenter = new Vector3((level - 1) / 2f, 0f, (level - 1) / 2f);
+
+        // 카메라가 바닥 위에 위치할 거리(distance)를 결정합니다.
+        float cameraDistance = level;
+
+        // 바닥의 크기를 기준으로 카메라가 바닥 위에서 얼마나 떨어져있어야 하는지 구합니다.
+        float cameraHeight = Mathf.Max(level, 5f) * 0.5f / Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
+
+        // 카메라 위치와 각도를 설정합니다.
+        Vector3 cameraPosition = new Vector3(floorCenter.x, cameraHeight * 2, floorCenter.z - cameraDistance);
+        Quaternion cameraRotation = Quaternion.Euler(60f, 0f, 0f);
+
+        // 카메라 위치 변경
+        Camera.main.transform.position = cameraPosition;
+        Camera.main.transform.rotation = cameraRotation;
+    }
+}
