43d35e376758bda6781466d8df9cc392823e9e6a
Goen Hee Han <gksrjsgml13@naver.com>
2023-05-03 13:35:44 +0900

    카메라 포지션 지정 위치 수정

--- Assets/Scripts/Manager/StageManager.cs
+++ Assets/Scripts/Manager/StageManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StageManager : Singleton<StageManager>
@@ -24,26 +25,6 @@ public class StageManager : Singleton<StageManager>
     {
         int floorSize = 2 * Lv + 1;
         floorLoader.CreateFloor(floorSize);
-        SetCameraPosition(floorSize);
-    }
-    
-    public void SetCameraPosition(int level)
-    {
-        // 바닥의 가운데 포지션을 구합니다.
-        Vector3 floorCenter = new Vector3((level - 1) / 2f, 0f, (level - 1) / 2f);
-
-        // 카메라가 바닥 위에 위치할 거리(distance)를 결정합니다.
-        float cameraDistance = level;
-
-        // 바닥의 크기를 기준으로 카메라가 바닥 위에서 얼마나 떨어져있어야 하는지 구합니다.
-        float cameraHeight = Mathf.Max(level, 5f) * 0.5f / Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
-
-        // 카메라 위치와 각도를 설정합니다.
-        Vector3 cameraPosition = new Vector3(floorCenter.x, cameraHeight * 2, floorCenter.z - cameraDistance);
-        Quaternion cameraRotation = Quaternion.Euler(60f, 0f, 0f);
-
-        // 카메라 위치 변경
-        Camera.main.transform.position = cameraPosition;
-        Camera.main.transform.rotation = cameraRotation;
+        Camera.main.AddComponent<CameraController>().SetCameraPosition(floorSize);
     }
 }
