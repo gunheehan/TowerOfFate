60b002b2e5bd44c247cf1bd85cbb0b9285b0d13d
Goen Hee Han <gksrjsgml13@naver.com>
2023-05-08 13:54:03 +0900

    타워 targetmanager 연결

--- Assets/Scripts/TowerObject.cs
+++ Assets/Scripts/TowerObject.cs
@@ -1,11 +1,37 @@
+using System;
 using UnityEngine;
 
 public class TowerObject : MonoBehaviour
 {
     private float power = 0f;
 
+    private FireMonster currentTarget = null;
+
+    private void OnEnable()
+    {
+        TargetManager.Instance.TargetReceived += UpdataTarget;
+    }
+
+    private void OnDisable()
+    {
+        TargetManager.Instance.TargetReceived -= UpdataTarget;
+    }
+
     private void Awake()
     {
         power = 10f;
+    }
+
+    private void Update()
+    {
+        if (currentTarget != null)
+        {
+            transform.LookAt(currentTarget.transform);
+        }
+    }
+
+    private void UpdataTarget(FireMonster target)
+    {
+        currentTarget = target;
     }
 }
