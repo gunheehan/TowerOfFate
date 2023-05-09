a5d4a5ec7b325b13c25b2bbfc7e9255400b600bf
Goen Hee Han <gksrjsgml13@naver.com>
2023-05-09 16:38:46 +0900

    towerState 임시 버튼 동작 구조 연결

--- Assets/Scripts/UI/UITowerState.cs
+++ Assets/Scripts/UI/UITowerState.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -13,11 +10,18 @@ public class UITowerState : MonoBehaviour, IUIInterface
 
     private List<FloorController> floorControllerList;
 
+    private Monster target = null;
+
     private void Start()
     {
         Btn_CheckPlaced.onClick.AddListener(OnClickCheckPlaced);
         Btn_FinishPlaced.onClick.AddListener(OnClickFinishPlaced);
         Btn_Monster.onClick.AddListener(OnClickMonster);
+    }
+
+    private void OnEnable()
+    {
+        TargetManager.Instance.TargetReceived += (value) => target = value;
     }
 
     public void SetFloorController(List<FloorController> floorControllers)
@@ -27,18 +31,21 @@ public class UITowerState : MonoBehaviour, IUIInterface
 
     private void OnClickCheckPlaced()
     {
-        foreach (FloorController floorController in floorControllerList)
-        {
-            floorController.CanPlaceOnFloor();
-        }
+        // foreach (FloorController floorController in floorControllerList)
+        // {
+        //     floorController.CanPlaceOnFloor();
+        // }
+        target.TakeDamage(100);
     }
 
     private void OnClickFinishPlaced()
     {
-        foreach (FloorController floorController in floorControllerList)
-        {
-            floorController.FinishPlaceFloor();
-        }
+        // foreach (FloorController floorController in floorControllerList)
+        // {
+        //     floorController.FinishPlaceFloor();
+        // }
+        StageManager.Instance.Roadtarget = null;
+        StageManager.Instance.OnLoadNextStage();
     }
 
     private void OnClickMonster()
