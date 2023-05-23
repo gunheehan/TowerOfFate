1427ea6a9ed0ca062c95674ece6c2c0b2b89462a
Goen Hee Han <gksrjsgml13@naver.com>
2023-05-24 00:32:56 +0900

    TowerObject.cs rotate base 오브젝트 회전으로 변경 및 BoundsController.cs 생성 로직 추가

--- Assets/Scripts/TowerObject.cs
+++ Assets/Scripts/TowerObject.cs
@@ -4,17 +4,12 @@ public class TowerObject : MonoBehaviour
 {
     private float power = 0f;
 
+    [SerializeField] private Transform TowerBase;
+
     private GameObject currentTarget = null;
+    private GameObject boundsObject = null;
 
-    private void OnEnable()
-    {
-        TargetManager.Instance.TargetReceived += UpdataTarget;
-    }
-
-    private void OnDisable()
-    {
-        TargetManager.Instance.TargetReceived -= UpdataTarget;
-    }
+    private bool isinit = false;
 
     private void Awake()
     {
@@ -25,12 +20,39 @@ public class TowerObject : MonoBehaviour
     {
         if (currentTarget != null)
         {
-            transform.LookAt(currentTarget.transform);
+            TowerBase.LookAt(currentTarget.transform);
         }
+    }
+    
+    private void OnEnable()
+    {
+        TargetManager.Instance.TargetReceived += UpdataTarget;
+    }
+
+    private void OnDisable()
+    {
+        TargetManager.Instance.TargetReceived -= UpdataTarget;
+    }
+
+    public void Init()
+    {
+        if (isinit)
+            return;
+
+        InstantiateBounds();
+        isinit = true;
     }
 
     private void UpdataTarget(IMonster target)
     {
         currentTarget = target.GetMonster();
+    }
+
+    private void InstantiateBounds()
+    {
+        boundsObject = new GameObject("BoundsObject"); // 새로운 게임 오브젝트 생성
+        boundsObject.transform.parent = transform; // 생성한 3D 객체의 자식으로 설정
+        
+        boundsObject.AddComponent<BoundsController>().SetBounds();
     }
 }
