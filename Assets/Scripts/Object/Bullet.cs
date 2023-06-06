e9f04d48cc38b4d8ecde018c28875d02acf933a5
Goen Hee Han <gksrjsgml13@naver.com>
2023-06-06 22:19:27 +0900

    bullet 추가

---
+++ Assets/Scripts/Object/Bullet.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+    private float speed = 10f;
+    private float arrivalDistance = 0.1f;
+    private Action onTargetReached;
+    private bool isSet = false;
+    
+    private void Update()
+    {
+        if(isSet)
+           Move(); 
+    }
+
+    private void OnDisable()
+    {
+        isSet = false;
+    }
+
+    private void Move()
+    {
+        if (target == null)
+        {
+            BulletManager.Instance.SpawnBullet(this);
+            return;
+        }            
+            
+        Vector3 direction = (target.position - transform.position).normalized;
+
+        float distance = speed * Time.deltaTime;
+
+        transform.Translate(direction * distance, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= arrivalDistance)
+        {
+            onTargetReached?.Invoke();
+        }
+    }
+
+    public void SetBullet(Transform targetobj, Action arriveAction)
+    {
+        target = targetobj;
+        onTargetReached = arriveAction;
+        gameObject.SetActive(true);
+        isSet = true;
+    }
+}
