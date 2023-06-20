3dc99997b0bf1055c83424cadc5cdf90bc803998
Goen Hee Han <gksrjsgml13@naver.com>
2023-06-20 18:00:11 +0900

    StageInfoTable Data Load 구조 변경

--- Assets/Scripts/StageInfoTable.cs
+++ Assets/Scripts/StageInfoTable.cs
@@ -1,12 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
 
-public class StageInfoTable : MonoBehaviour
+public enum MonsterType
 {
+    Normal,
+    Fire
+}
+public struct StageDate
+{
+    public int Level;
+    public int FloorLevel;
+    public float PlayTime;
+    public float ReLoadTime;
+    public MonsterType MonsterType;
+    public int MonsterAmount;
+}
+public class StageInfoTable
+{
+    private List<StageDate> stageDataList = new List<StageDate>();
 
-    public void LoadSheetData()
+    public List<StageDate> StageDataList
+    {
+        get
+        {
+            if(stageDataList.Count <= 0)
+                LoadData();
+
+            return stageDataList;
+        }
+    }
+
+    private void LoadData()
     {
         string folderPath = "Assets/DataSheets/";
         string[] csvFiles = Directory.GetFiles(folderPath,  "StageInfoTable.csv");
@@ -17,6 +44,37 @@ public class StageInfoTable : MonoBehaviour
             string[] key = fileName.Split('_');
 
             string csvData = File.ReadAllText(csvFile);
+            DataParsing(csvData);
+        }
+    }
+
+    private void DataParsing(string data)
+    {
+        string[] downloadData_split = data.Split("\r\n");
+        int i = 1;
+        try
+        {
+            for (i = 1; i < downloadData_split.Length; i++)
+            {
+                string[] _data = downloadData_split[i].Split(",");
+
+                if (!string.IsNullOrEmpty(_data[0]))
+                {
+                    stageDataList.Add(new StageDate()
+                    {
+                        Level = int.Parse(_data[0]),
+                        FloorLevel = int.Parse(_data[1]),
+                        PlayTime = float.Parse(_data[2]),
+                        ReLoadTime = float.Parse(_data[3]),
+                        MonsterType = (MonsterType)int.Parse(_data[4]),
+                        MonsterAmount = int.Parse(_data[5])
+                    });
+                }
+            }
+        }
+        catch
+        {
+            Debug.Log("StageInfoTable Parsing Error");
         }
     }
 }
